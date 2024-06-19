6f54d2eceb379f908abba57f521d405e39c17e46
Vladimir Dmitriev <gdvsbook@gmail.com>
2024-06-19 21:58:55 +0300

    Tasks completed

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,21 @@
-// @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
-// @todo: DOM узлы
+const placesList = document.querySelector('.places__list');
 
-// @todo: Функция создания карточки
+initialCards.forEach(createCard);
 
-// @todo: Функция удаления карточки
+function createCard(card) {
+    const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+    cardElement.querySelector('.card__image').src = card.link;
+    cardElement.querySelector('.card__title').textContent = card.name;
+    cardElement.querySelector('.card__image').alt = card.name;
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+    deleteButton.addEventListener('click', removeCard);
+    placesList.append(cardElement);
+}
 
-// @todo: Вывести карточки на страницу
+function removeCard(evt) {
+    const eventTarget = evt.target;
+    const deleteCard = eventTarget.parentElement;
+    deleteCard.remove();
+}
